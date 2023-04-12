c5e66d6fa252685e9dd01949311b671da7a84bda
Yota <yota.reco@gmail.com>
2023-04-13 00:47:38 +0530

    route changes

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Mail;
+use Illuminate\Support\Str;
+
 
 class MailController extends Controller
 {
-    //
+     //SendEmail
+
+     public function testSend(){
+        return "testing";
+     }
+
+     public function sendVerificationCode(Request $request)
+     {
+         $data = array('name' => 'Music App Verification');
+         $user = Auth::user();
+         $code = Str::random(6);
+         Mail::raw("welcome to verification center $code is your code",function ($m) use($user)  {
+             $m->from('fathimahafsa97101@outlook.com', 'Music App Verification')
+             ->to($user->email, $user->name)->subject('Verification Message!');
+         });
+             return response(['code'=>1, 'message'=> 'Email has been sent', 'verification_code'=>$code]);
+     
+     }   
+
 }
