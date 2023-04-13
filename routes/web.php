cedd69173ae0a95e5919b42b6dc53bb326728bf1
nmssalman <nmssalman@outlook.com>
2023-04-13 06:30:09 +0530

    route changes

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::prefix('v1/')->group(function(){
+
+    Route::prefix('user/')->group(function(){
+        Route::post('/', [UserController::class, 'RegisterUser']);
+        Route::get('/', [UserController::class, 'LoginUser']);
+
+        Route::middleware('auth:api')->group(function() {
+            Route::delete('/', [UserController::class, 'DeleteUser']);
+            Route::patch('/', [UserController::class, 'UpdateUserInfo']);
+            Route::get('/get-users', [UserController::class, 'GetAllUsers']);
+            Route::get('/is-verified-email',[UserController::class,'IsEmailVerified']);
+            Route::get('/is-verified-phone',[UserController::class,'IsPhoneVerified']);
+            Route::put('/verify-email',[UserController::class,'VerifyEmail']);
+            Route::patch('/verify-phone',[UserController::class,'VerifyPhone']);
+            Route::get('/send-email',[MailController::class,'SendEmail']);
+            
+    
+        });
+    });
+    Route::prefix('song/')->group(function(){
+
+        Route::middleware('auth:api')->group(function() {
+    
+            Route::post('/',[TrackController::class,'CreateRecords']);
+            Route::get('/',[TrackController::class,'RetrieveData']);
+        
+        });
+     
+    });
+    
+});
+
 // Route::get('/', [UserController::class, 'LoginUser']);
 
